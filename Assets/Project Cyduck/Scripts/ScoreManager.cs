5074c1988e5b15c3b9e0f164fe73ec44fef4120e
Malik Ariefa <malik.ariefa@gmail.com>
2017-08-13 14:09:06 +0700

    + Added score manager and time manager

---
+++ Assets/Project Cyduck/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : Singleton<TimeManager> {
+
+	protected ScoreManager () {}
+
+	public Text scoreViewer;
+
+	private int gameScore;
+	public int score {
+		get {
+			return gameScore;
+		}
+	}
+
+	public void AddScore(int added) {
+		gameScore += added;
+	}
+
+	void Update() {
+		if (scoreViewer != null) {
+			scoreViewer.text = gameScore.ToString();
+		}
+	}
+}
---
+++ Assets/Project Cyduck/Scripts/TimeManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : Singleton<TimeManager> {
+
+	protected TimeManager () {}
+
+	public int initialTime;
+	public Text textViewer;
+
+	private float remaining; 
+	public int remainingTime {
+		get {
+			return Mathf.FloorToInt (remaining);
+		}
+		set {
+			remaining = value;
+		}
+	}
+
+	void Awake() {
+		remaining = (float) initialTime + 1.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		remaining -= Time.deltaTime;
+		if (IsTimeUp()) {
+			remaining = 0f;
+		}
+
+		if (textViewer != null) {
+			textViewer.text = remainingTime.ToString ();
+		}
+	}
+
+	public void AddRemainingTime(int addTime) {
+		remaining += addTime;
+	}
+
+	public bool IsTimeUp() {
+		return remaining > 0f ? false : true;
+	}
+}
